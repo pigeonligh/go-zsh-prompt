155df9032bfdaa0999733b681d6d6bb6815abe0c
chenxu1 <chenxu1@sensetime.com>
2023-10-20 16:04:42 +0800

    update

--- examples/main.go
+++ examples/main.go
@@ -13,19 +13,24 @@ func main() {
 	home, err := filepath.Abs(".home")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	prompt := gozshprompt.New(
-		"zsh",
+	prompt, err := gozshprompt.NewFromPath(
 		gozshprompt.WithHome(home),
 		gozshprompt.WithHandler(func(ctx context.Context, input string) error {
 			fmt.Printf("Solve %v\n", input)
 			return nil
 		}),
 	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
 	err = prompt.Run(context.Background())
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
